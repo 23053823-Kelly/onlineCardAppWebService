3ff300270c7c35c6873fcf7b85b247e099c12d4f
23053823-Kelly <23053823@myrp.edu.sg>
2026-01-07 17:43:06 +0800

    completeddd

--- server.js
+++ server.js
@@ -39,11 +39,13 @@ app.get('/allcards',async (req,res)=>{
     }
 });
 
+//Example route: create a new card
+
 app.post('/addcard',async (req,res)=>{
     const {card_name, card_pic} =req.body;
-    try{
+    try {
         let connection = await mysql.createConnection(dbConfig);
-        await.connection.execute('INSERT INTO cards (card_name, card_pic) VALUES (?,?)',[card_name,card_pic]);
+        await connection.execute('INSERT INTO cards (card_name, card_pic) VALUES (?,?)',[card_name,card_pic]);
         res.status(201).json({message: 'Card'+card_name+' added successfully.'});
     } catch (err) {
         console.error(err);
